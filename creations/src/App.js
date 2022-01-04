dceab442ba7ead7415f2b9174d278c3ae0d2a737
UmangLadha <umangladha02@gmail.com>
2022-01-05 02:08:30 +0530

    added files

--- creations/src/App.js
+++ creations/src/App.js
@@ -1,58 +1,36 @@
 import './App.css';
 import logo from './images/logo.jpg';
-import Carousel from 'react-bootstrap/Carousel';
-
+import Slider from './components/Slider.js';
+import Services from './components/Services.js';
+import Clients from './components/Clients.js';
+import About from './components/About.js';
+import Contact from './components/Contact.js';
+// import Carousel from 'react-bootstrap/Carousel';
 
 function App() {
   return (
-    <div className="app">
+     <div className="app">
       <div>
-        <div className="header"><img className="logo" src={logo} alt="logo"/></div>
-        {/* <div class="nav-brand"><img src={logo} alt="logo"/></div> */}
+        <div className="header"><img className="logo" src={logo} alt="logo" /></div>
+        <Slider />
+        <nav className="navigation">
+          <ul className="list-non-bullet list">
+            <li className="list-inline"><a className="toplink" href="http:#"> SERVICE </a></li>
+            <li className="list-inline"><a className="toplink" href="http:#"> CLIENT </a></li>
+            <li className="list-inline"><a className="toplink" href="http:#"> REVIEW </a></li>
+            <li className="list-inline"><a className="toplink" href="http:#"> GALLERY </a></li>
+            <li className="list-inline"><a className="toplink" href="http:#"> ABOUT US </a></li>
+            <li className="list-inline"><a className="toplink" href="#contact"> CONTACT </a></li>
+          </ul>
+        </nav>
 
-    <div className="slider">
-
-        <Carousel variant="dark">
-      <Carousel.Item>
-        <img
-          className=" w-100 slider-img "
-          src={logo}
-          alt="First slide"
-        />
-      </Carousel.Item>
-
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={logo}
-          alt="Second slide"
-        />
-      </Carousel.Item>
-
-      <Carousel.Item>
-        <img
-          className="d-block w-100 "
-          src={logo}
-          alt="Third slide"
-        />
-      </Carousel.Item>
-     </Carousel>
+      </div>
+      <Services/>
+      <Clients />
+      <About />
+      <Contact />
     </div>
-           <div className="navigation">
-           <div class="nav-brand"><img src={logo} alt="logo"/></div>
-            <ul className="list-non-bullet list">
-                <li className="list-inline"> <a href="http:#">SERVICE </a> |</li>
-                <li className="list-inline"><a href="http:#"> CLIENT </a> |</li>
-                <li className="list-inline"><a href="http:#"> REVIEW </a> |</li>
-                <li className="list-inline"><a href="http:#"> GALLERY </a> |</li>
-                <li className="list-inline"><a href="http:#"> ABOUT US </a> |</li>
-                <li className="list-inline"><a href="#contact"> CONTACT </a></li>
-            </ul> 
-           </div>  
-      </div>      
-    </div>
-
   );
 }
-
 export default App;
+ 
--- creations/src/components/contact.js
+++ creations/src/components/contact.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import '../App.css';
 
-function contact() {
+function Contact() {
     return (
-        <div>
-            <h1 id="contact">CONTACT</h1>
-        </div>
+    <div> 
+        <footer className="footer">
+          <h1>CONTACT</h1>
+            <div className="cards">
+                <div className="card">
+                    <p>49,shankar colony</p>
+                    <p>jaipur, Rajasthan,302039</p>
+                    <span>_____</span>
+                    <p>Tel: 7597226305</p>
+                    <p>Email: umangladha02@gmail.com</p>
+                    <ul className="list-non-bullet" >
+                        <li className="list-inline"><a className="toplinks" href="http//:#"><i class="fa fa instagram">I</i></a></li>
+                        <li className="list-inline"><a className="toplinks" href="http//:#"><i class="fa fa facebook">F</i></a></li>
+                        <li className="list-inline"><a className="toplinks" href="http//:#"><i class="fa fa gmail">G</i></a></li>
+                    </ul>
+
+                </div>
+
+                <div className="card">
+                    <p>Opening hours</p>
+                    <span>_____</span>
+                    <p>Mon-Fri: 9AM-8PM</p>
+                    <p>Saturday: 9AM-7PM</p>
+                    <p>Sunday: 10AM-6PM</p>
+                </div>
+
+            </div>
+        </footer>
+    </div>
     )
 }
-
-export default contact;
+export default Contact;
