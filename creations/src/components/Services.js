a821d324e54cdde2e0d92215911b7acb6d4f13bf
UmangLadha <umangladha02@gmail.com>
2022-01-06 00:03:46 +0530

    some Changes

--- creations/src/components/Services.js
+++ creations/src/components/Services.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import '../App.css';
+import Figure from 'react-bootstrap/Figure';
+import logo from '../images/logo.jpg';
+
 
 function services() {
     return (
-        <div>
-            
-        </div>
+        <Figure className='details'>
+            <Figure.Image
+                width={350}
+                height={350}
+                alt="200x200"
+                src={logo}
+            />
+            <Figure.Image
+                width={350}
+                height={350}
+                alt="171x180"
+                src={logo}
+            />
+            <Figure.Image
+                width={350}
+                height={350}
+                alt="171x180"
+                src={logo}
+            />
+            <Figure.Image
+                width={350}
+                height={350}
+                alt="171x180"
+                src={logo}
+            />
+        </Figure>
     )
 }
 
